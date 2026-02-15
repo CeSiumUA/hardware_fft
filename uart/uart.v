8efa50d198f28ab9b74a75c49d13786de738f174
CeSium <fedir.katushonok@gmail.com>
2026-02-15 09:32:19 +0200

    Implemented UART

---
+++ uart/uart.v
@@ -0,0 +1,8 @@
+////////////////////////////////////////////////////////
+// RS-232 RX and TX modules
+// (c) fpga4fun.com & KNJN LLC - 2003 to 2016
+
+`include "async_transmitter.v"
+`include "async_receiver.v"
+`include "baud_tick_gen.v"
+
