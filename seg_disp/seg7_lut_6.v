db6b9ab8048c416e7f2c16bae278cbdfeaa38267
CeSium <fedir.katushonok@gmail.com>
2026-02-15 10:45:52 +0200

    Update SEG7 LUT modules to use 8-bit output for improved display functionality

--- seg_disp/seg7_lut_6.v
+++ seg_disp/seg7_lut_6.v
@@ -1,6 +1,6 @@
 module SEG7_LUT_6 (	oSEG0,oSEG1,oSEG2,oSEG3,oSEG4,oSEG5,iDIG );
 input	[23:0]	iDIG;
-output	[6:0]	oSEG0,oSEG1,oSEG2,oSEG3,oSEG4,oSEG5;
+output	[7:0]	oSEG0,oSEG1,oSEG2,oSEG3,oSEG4,oSEG5;
 
 SEG7_LUT	u0	(	oSEG0,iDIG[3:0]		);
 SEG7_LUT	u1	(	oSEG1,iDIG[7:4]		);
