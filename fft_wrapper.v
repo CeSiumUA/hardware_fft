8fd1d6db923fc5880a324d6c43ec0ddaab7a3337
CeSium <fedir.katushonok@gmail.com>
2026-02-18 07:55:55 +0200

    Add dual port RAM VHDL file and update FFT wrapper for parameterization

--- fft_wrapper.v
+++ fft_wrapper.v
@@ -1,11 +1,13 @@
-module fft_wrapper(clk, in_signal, real_power, imag_power, fft_source_sop, sink_sop, sink_eop, sink_valid, reset_n);
+module fft_wrapper #(
+    parameter integer FFT_BITS = 14
+)(clk, in_signal, real_power, imag_power, fft_source_sop, sink_sop, sink_eop, sink_valid, reset_n);
 
 input clk;
-input wire [13:0] in_signal;
+input wire [FFT_BITS-1:0] in_signal;
 wire [31:0] short_in_signal;
 
-output wire [24:0] real_power;
-output wire [24:0] imag_power;
+output wire [FFT_BITS-1:0] real_power;
+output wire [FFT_BITS-1:0] imag_power;
 
 output wire sink_valid;
 wire sink_ready;
@@ -15,8 +17,8 @@ wire [10:0] fft_pts;
 output wire fft_source_sop;
 wire fft_source_eop;
 
-wire [13:0] real_to_fft_p;
-wire [13:0] imag_to_fft_p;
+wire [FFT_BITS-1:0] real_to_fft_p;
+wire [FFT_BITS-1:0] imag_to_fft_p;
 reg [4:0] count;
 output reg reset_n;
 reg eop2, sop2, eop5;
@@ -48,26 +50,41 @@ control_for_fft control_for_fft_longer_inst(
     .fft_pts(fft_pts)
 );
 
-fft fft_inst(
-    .clk(clk),
-    .reset_n(reset_n),
-    .sink_valid(sink_valid),
-    .sink_ready(sink_ready),
-    .sink_error(2'b00),
-    .sink_sop(sink_sop),
-    .sink_eop(sink_eop),
-    .sink_real(real_to_fft_p),
-    .sink_imag(imag_to_fft_p),
-    .fftpts_in(fft_pts),
+fft #(
+    .BITS(FFT_BITS)
+) fft_inst(
+    .clock(clk),
+    .reset(reset_n),
+    // .sink_valid(sink_valid),
+    // .sink_ready(sink_ready),
+    // .sink_error(2'b00),
+    // .sink_sop(sink_sop),
+    // .sink_eop(sink_eop),
+    // .sink_real(real_to_fft_p),
+    // .sink_imag(imag_to_fft_p),
+    // .fftpts_in(fft_pts),
+    // .inverse(1'b0),
+    // .source_valid(),
+    // .source_ready(1'b1),
+    // .source_error(),
+    // .source_sop(fft_source_sop),
+    // .source_eop(fft_source_eop),
+    // .source_real(real_power),
+    // .source_imag(imag_power),
+    // .fftpts_out()
     .inverse(1'b0),
-    .source_valid(),
-    .source_ready(1'b1),
-    .source_error(),
-    .source_sop(fft_source_sop),
-    .source_eop(fft_source_eop),
-    .source_real(real_power),
-    .source_imag(imag_power),
-    .fftpts_out()
+    .in_real(real_to_fft_p),
+    .in_imag(imag_to_fft_p),
+    .in_valid(sink_valid),
+    .in_sop(sink_sop),
+    .in_eop(sink_eop),
+
+    .out_real(real_power),
+    .out_imag(imag_power),
+    .out_error(),
+    .out_valid(),
+    .out_sop(fft_source_sop),
+    .out_eop(fft_source_eop)
 );
 
 endmodule
